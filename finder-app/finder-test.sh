3439e5fa0e3a57922abdc7002b530f07a7b0ab2d
Dexatr <105407609+Dexatr@users.noreply.github.com>
2024-08-24 09:53:10 -0400

    Update finder-test.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -10,12 +10,12 @@ WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
 
 # Ensure the conf directory and its files exist
-if [ ! -f conf/username.txt ] || [ ! -f conf/assignment.txt ]; then
-    echo "Missing conf/username.txt or conf/assignment.txt"
+if [ ! -f /etc/finder-app/conf/username.txt ] || [ ! -f /etc/finder-app/conf/assignment.txt ]; then
+    echo "Missing /etc/finder-app/conf/username.txt or /etc/finder-app/conf/assignment.txt"
     exit 1
 fi
 
-username=$(cat conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -39,7 +39,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # Check the assignment type
-assignment=$(cat conf/assignment.txt)
+assignment=$(cat /etc/finder-app/conf/assignment.txt)
 
 if [ $assignment != 'assignment1' ]
 then
@@ -53,16 +53,19 @@ then
     fi
 fi
 
-# Clean and compile the writer application
+# Clean and compile the writer application if necessary
 # make clean
 # make
 
 for i in $(seq 1 $NUMFILES)
 do
-    ./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+    /usr/bin/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(/usr/bin/finder.sh "$WRITEDIR" "$WRITESTR")
+
+# Write output to /tmp/assignment4-result.txt
+echo "${OUTPUTSTRING}" > /tmp/assignment4-result.txt
 
 # Remove temporary directories
 rm -rf /tmp/aeld-data
